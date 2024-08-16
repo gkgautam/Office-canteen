8403f50f603a95ebb4341aa249870506b5ece432
govind.govind <govind.govind@cvent.com>
2024-08-16 21:49:26 +0530

    creating User layout

---
+++ src/app/(user)/user/home/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Cards from '../../components/cards/Cards'
+import { getMenu } from '@/actions/menus/fetchmenu/route'
+
+async function UserHome() {
+
+     const allData = await getMenu();
+
+  return (
+    <>
+    {/* Card Blog */}
+    <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto border-2">
+      {/* Grid */}
+      <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {/* Card */}
+        {
+            allData.data?.map((menuItem,index)=>{
+                return <Cards data = {menuItem as any} key={index} />
+            })
+        }
+            
+      </div>
+      {/* End Grid */}
+    </div>
+    {/* End Card Blog */}
+  </>
+  
+  )
+}
+
+export default UserHome
